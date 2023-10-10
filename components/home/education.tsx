1c21989ee47bb308d5995621b8f3d6d2c66a5c1d
Irfan Fakhri <m.irfan.fakhri66@gmail.com>
2023-10-10 15:28:46 +0700

    education

---
+++ components/home/education.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+
+import {GraduationCap} from "lucide-react";
+
+export default function Education() {
+  return (
+    <section className="relative lg:py-20 bg-white/[10%]">
+      <div className="container">
+        <div className="flex flex-col sm:flex-row w-full sm:gap-32 items-start">
+          <div className="flex items-center mr-auto">
+            <GraduationCap className="h-10 w-10 mr-4"/>
+            <h3 className="text-5xl font-bold mr-auto">
+              Education
+            </h3>
+          </div>
+          <div className="flex flex-col">
+            <img src="/education/smkn4.png" alt="SMK Negeri 4 Bandung" className="w-28 h-28 mb-2"/>
+            <p className="tracking-wide">SMK Negeri 4 Bandung</p>
+            <p className="text-gray-400 font-light">Vocational School</p>
+            <p>Software Engineering</p>
+            <p className="">2018</p>
+          </div>
+          <div className="flex flex-col">
+            <img src="/education/unikom.png" alt="Universitas Komputer Indonesia" className="w-28 h-28 mb-2"/>
+            <p className="tracking-wide">Universitas Komputer Indonesia</p>
+            <p className="text-gray-400 font-light">Bachelor Degree</p>
+            <p>Information System</p>
+            <p className="">2023</p>
+          </div>
+        </div>
+      </div>
+
+    </section>
+  )
+}
