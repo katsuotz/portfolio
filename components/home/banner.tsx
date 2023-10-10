8ea83dde68a49856b4ef65aeffa70c72f52dae35
Irfan Fakhri <m.irfan.fakhri66@gmail.com>
2023-10-10 15:27:47 +0700

    change icon library

--- components/home/banner.tsx
+++ components/home/banner.tsx
@@ -1,23 +1,16 @@
-import {
-  ArrowDownIcon,
-  CircleIcon, Crosshair1Icon,
-  EnvelopeClosedIcon,
-  GitHubLogoIcon,
-  InstagramLogoIcon,
-  LinkedInLogoIcon,
-} from "@radix-ui/react-icons";
 import Id from "@/components/home/id";
+import {ArrowDownCircle, GithubIcon, InstagramIcon, LinkedinIcon, MailIcon, MapIcon} from "lucide-react";
 
 export default function Banner() {
   const year = new Date().getFullYear()
   const startYear = 2018
 
   return (
-    <section className="h-screen w-screen relative flex justify-center items-center">
-      <Id className="fixed -z-10 opacity-20 w-[1000px]"/>
+    <section className="h-screen w-full relative flex justify-center items-center">
+      <Id className="absolute -z-10 opacity-20 w-[1000px]"/>
 
       <div>
-        <h1 className="text-[190px] font-black leading-[160px] shadow tracking-wide">
+        <h1 className="text-[190px] font-black leading-[160px] shadow tracking-wider">
           <span className="text-white/[0.5]">Irfan</span>
           <span className="block">Fakhri</span>
         </h1>
@@ -28,25 +21,24 @@ export default function Banner() {
         </p>
         <div className="flex gap-4 mt-6">
           <a href="https://github.com/katsuotz/" target="_blank">
-            <GitHubLogoIcon className="w-6 h-6"/>
+            <GithubIcon className="w-6 h-6"/>
           </a>
           <a href="https://www.linkedin.com/in/irfan-fakhri/" target="_blank">
-            <LinkedInLogoIcon className="w-6 h-6"/>
+            <LinkedinIcon className="w-6 h-6"/>
           </a>
           <a href="https://www.instagram.com/katsuotz/" target="_blank">
-            <InstagramLogoIcon className="w-6 h-6"/>
+            <InstagramIcon className="w-6 h-6"/>
           </a>
         </div>
         <div className="flex justify-center mt-14">
           <div className="relative w-8-h-8 flex justify-center items-center cursor-pointer motion-reduce:animate-bounce">
-            <CircleIcon className="w-8 h-8 fixed stroke-1"/>
-            <ArrowDownIcon className="w-4 h-4"/>
+            <ArrowDownCircle className="w-8 h-8"/>
           </div>
         </div>
 
         <div className="absolute top-0 left-0 pl-10 pt-10">
-          <p className="flex items-center"><EnvelopeClosedIcon className="w-4 h-4 mr-2"/> m.irfan.fakhri66@gmail.com</p>
-          <p className="flex items-center"><Crosshair1Icon className="w-4 h-4 mr-2"/> Indonesia</p>
+          <p className="flex items-center"><MailIcon className="w-4 h-4 mr-2"/> m.irfan.fakhri66@gmail.com</p>
+          <p className="flex items-center"><MapIcon className="w-4 h-4 mr-2"/> Indonesia</p>
         </div>
       </div>
     </section>
