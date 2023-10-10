d6c6b3039720a15db791ea474cbe7c27685c5260
Irfan Fakhri <m.irfan.fakhri66@gmail.com>
2023-10-10 15:28:57 +0700

    home

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Banner from "@/components/home/banner";
+import WorkExperience from "@/components/home/work-experience";
+import Education from "@/components/home/education";
+import Achievement from "@/components/home/achievement";
 
 export default function Home() {
   return (
     <div>
       <Banner/>
+      <WorkExperience/>
+      <Education/>
+      <Achievement/>
     </div>
   )
 }
