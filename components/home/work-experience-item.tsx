26b4ec017e0273e14e8283fb06f3f2189c5895d0
Irfan Fakhri <m.irfan.fakhri66@gmail.com>
2023-10-10 19:04:30 +0700

    fix work margin

--- components/home/work-experience-item.tsx
+++ components/home/work-experience-item.tsx
@@ -15,7 +15,7 @@ export interface WorkExperienceType {
 export default function WorkExperienceItem({experience}: { experience:WorkExperienceType }) {
   return (
     <div
-      className="flex mx-2 flex-col items-center justify-center px-4 py-6 text-center bg-white/[10%] rounded-lg w-[450px]">
+      className="flex flex-col items-center justify-center px-4 py-6 text-center bg-white/[10%] rounded-lg w-[450px]">
       <Image src={experience.logo} alt="" width={40} height={40} className="w-auto h-10 mb-1.5"/>
       <p className="mb-2 tracking-wider">{experience.company}</p>
       <div className="flex gap-6">
