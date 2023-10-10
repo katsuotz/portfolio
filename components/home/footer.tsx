59aae59559684e4442d40d2a7ba46fa4366485af
Irfan Fakhri <m.irfan.fakhri66@gmail.com>
2023-10-10 18:52:29 +0700

    add footer

---
+++ components/home/footer.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function Footer() {
+  return (
+    <footer className="relative flex flex-col justify-center items-center lg:pt-20 pt-12 lg:pb-20 pb-12">
+      <p className="text-gray-400 tracking-wide">&copy; 2023 <span className="text-white font-medium">Irfan Fakhri</span>. katsuotz.id</p>
+    </footer>
+  )
+}
