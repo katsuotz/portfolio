ef4f17fc5e1ba992b272dd4ea2348c6ed3572865
Irfan Fakhri <m.irfan.fakhri66@gmail.com>
2023-10-12 12:10:56 +0700

    fix back to top size

--- components/home/back-to-top.tsx
+++ components/home/back-to-top.tsx
@@ -22,7 +22,7 @@ export default function BackToTop() {
   return (
     <ArrowUpSquareIcon
       onClick={scrollToTop}
-      className={`w-8 h-8 cursor-pointer fixed bottom-10 right-10 z-10 back-to-top transition-all ${showBackToTop ? '' : 'opacity-0 pointer-events-none'}`}
+      className={`w-10 h-10 cursor-pointer fixed bottom-10 right-10 z-10 back-to-top transition-all ${showBackToTop ? '' : 'opacity-0 pointer-events-none'}`}
     />
   )
 }
