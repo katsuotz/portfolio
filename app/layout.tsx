ecab881dfdafaed7582c37d6c5b465c9346cc12d
Irfan Fakhri <m.irfan.fakhri66@gmail.com>
2023-12-28 14:46:28 +0700

    test meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,12 +20,8 @@ export default function RootLayout(
   return (
     <html lang="en">
     <head>
-      <title>{metadata.title}</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-      <meta name="description" content={metadata.description}/>
-      <meta name="keywords" content={metadata.keywords}/>
-      <meta name="author" content={metadata.author}/>
     </head>
     <body className={outfit.className}>
     <main className="relative">
