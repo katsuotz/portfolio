f3d7cf7dbdefdfaee5305a302980442cde653ace
Irfan Fakhri <m.irfan.fakhri66@gmail.com>
2023-12-28 15:00:53 +0700

    fix metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,9 @@ const outfit = Outfit({subsets: ['latin']})
 export const metadata = {
   title: 'Irfan Fakhri - Software Engineer | Katsuotz',
   description: 'Experienced full-stack developer with over 5 years of industry expertise, actively engaged in startups and freelancing on side projects.',
-  author: 'Irfan Fakhri',
-  keywords: 'web design, web, web apps, html, css, js, ts, node.js, next, vue, react, go, golang, php, laravel, mongodb, sql, mysql, postgre'
+  keywords: 'web design, web, web apps, html, css, js, ts, node.js, next, vue, react, go, golang, php, laravel, mongodb, sql, mysql, postgre',
+  authors: [{ name: 'Irfan Fakhri' }, { name: 'Katsuotz' }],
+  viewport: 'width=device-width, initial-scale=1.0',
 }
 
 export default function RootLayout(
@@ -20,7 +21,6 @@ export default function RootLayout(
   return (
     <html lang="en">
     <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <meta httpEquiv="x-ua-compatible" content="ie=edge" />
     </head>
     <body className={outfit.className}>
