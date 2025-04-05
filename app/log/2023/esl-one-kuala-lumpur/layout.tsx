9805a87ea06e6c9df558779ff76646bd9f9d29bd
Irfan Fakhri <m.irfan.fakhri66@gmail.com>
2025-04-06 00:15:36 +0700

    chore: update metadata

--- app/log/2023/esl-one-kuala-lumpur/layout.tsx
+++ app/log/2023/esl-one-kuala-lumpur/layout.tsx
@@ -2,7 +2,7 @@ export const metadata = {
   title: 'ESL One Kuala Lumpur 2023 | Katsuotz',
   description: 'ESL One Kuala Lumpur 2023 Experience.',
   keywords:
-    'esl, esl one, esl one kl, esl one kuala lumpur, kuala lumpur, malaysia',
+    'esl, esl one, esl one kl, esl one kuala lumpur, kuala lumpur, malaysia, dota, dota 2',
   authors: [{ name: 'Muhammad Irfan Fakhri' }, { name: 'Katsuotz' }],
 }
 
